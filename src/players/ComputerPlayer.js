ebdca23974fcf7d7cc1b28202b4f4a8842151bf0
IlyaSaenko <mr.journalist@inbox.ru>
2026-01-12 12:20:01 +0300

    feat: improved AI logic

--- src/players/ComputerPlayer.js
+++ src/players/ComputerPlayer.js
@@ -1,13 +1,4 @@
 import AbstractPlayer from "./AbstractPlayer.js";
-//Создаём класс, а в нём async метод
-// 1️⃣ Делаем небольшую паузу, чтобы ход компьютера выглядел "человечным"
-// await заставляет игру подождать, пока "компьютер думает"
-// 2️⃣ Получаем список всех ПУСТЫХ клеток на игровом поле
-// Метод внутри board возвращает массив индексов свободных ячеек
-// 3️⃣ Случайным образом выбираем одну из свободных клеток
-// Это и будет ход компьютера — простой рандомный бот
-// 4️⃣ Возвращаем выбранный индекс игре
-// Game.js продолжит ход: поставит символ, проверит победу и т.д.
 
 export default class ComputerPlayer extends AbstractPlayer {
   async getMove(board) {
@@ -15,6 +6,27 @@ export default class ComputerPlayer extends AbstractPlayer {
       setTimeout(resolve, 2000)
     });
     const emptyIndices = board.getEmptyIndices();
+    
+    for (let idx of emptyIndices) {
+      board.state[idx] = this.marker;
+      if (board.checkWin(this.marker)) {
+        board.state[idx] = '';
+        return idx;
+      }
+      board.state[idx] = '';
+    }
+
+    const playerMarker = this.marker === 'X' ? 'O' : 'X';
+    for (let idx of emptyIndices) {
+      board.state[idx] = playerMarker;
+      if (board.checkWin(playerMarker)) {
+        board.state[idx] = '';
+        return idx;
+      }
+      board.state[idx] = '';
+    }
+
+
     return emptyIndices[Math.floor(Math.random() * emptyIndices.length)];
   }
 }
